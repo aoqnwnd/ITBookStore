1d4362e6f88d342b16edc0bb571562cd20ec8ce6
Jacob_Kim <71866565+aoqnwnd@users.noreply.github.com>
2024-03-15 12:18:42 +0900

    [fix] Binds 어노테이션 -> Provides 어노테이션
    - 이유 : @Binds 어노테이션을 사용하는 class는 무조건 abstract class여야지만 hilt 측에서 제대로 처리할 수 있음
    > 추가로 실제 구현을 가지고 있는 class를 사용하면 안 된다.
    > 이유
    -  인터페이스와 구현 클래스 간의 매핑만을 정의
    - @binds 클래스가 구현 코드를 포함하면 변경이 어려워지고 의존성이 발생한다.
    - @Module 클래스는 DI 그래프 구성을 위한 메타데이터만 제공해야 하며, 실제 로직은 포함하지 않습니다.

--- core/data/src/main/java/com/devkick/data/di/RepositoryModule.kt
+++ core/data/src/main/java/com/devkick/data/di/RepositoryModule.kt
@@ -4,13 +4,14 @@ import com.devkick.data.repository.BookRepository
 import com.devkick.data.repository.impl.BookRepositoryImpl
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
 interface RepositoryModule {
-    @Binds
+    @Provides
     fun bindsBookRepository(
         bookRepositoryImpl: BookRepositoryImpl
     ): BookRepository
